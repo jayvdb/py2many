32a4d2d753c835d961ab6a24213707c54de3b783
John Vandenberg <jayvdb@gmail.com>
2021-04-11 13:24:11 +0800

    Update tests

--- tests/cases/int_enum.py
+++ tests/cases/int_enum.py
@@ -11,3 +11,16 @@ class Permissions(IntFlag):
     R = 1
     W = 2
     X = 16
+
+
+def show():
+	for color in Colors:
+		print(color)
+		print(color.value)
+	for perm in Permissions:
+		print(perm)
+		print(perm.value)
+
+
+if __name__ == "__main__":
+	show()
--- tests/cases/str_enum.py
+++ tests/cases/str_enum.py
@@ -1,7 +1,17 @@
-from enum import Enum, IntFlag, auto
+from enum import Enum
 
 
 class Colors(str, Enum):
     RED = "red"
     GREEN = "green"
     BLUE = "blue"
+
+
+def show():
+	for color in ColorNames:
+		print(color)
+		print(color.value)
+
+
+if __name__ == "__main__":
+	show()
