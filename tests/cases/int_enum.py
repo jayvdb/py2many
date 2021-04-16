36dc2278142d03d28b5ac52b974ef30be45a8888
John Vandenberg <jayvdb@gmail.com>
2021-04-17 03:43:58 +0800

    More

--- tests/cases/int_enum.py
+++ tests/cases/int_enum.py
@@ -7,7 +7,20 @@ class Colors(IntEnum):
     BLUE = auto()
 
 
-class Permissions(IntFlag):
-    R = 1
-    W = 2
-    X = 16
+#class Permissions(IntFlag):
+#    R = 1
+#    W = 2
+#    X = 16
+
+
+def show():
+    print(Colors.RED.value)
+    print(Colors.GREEN.value)
+    print(Colors.BLUE.value)
+    #for perm in Permissions:
+    #    print(perm)
+    #    print(perm.value)
+
+
+if __name__ == "__main__":
+    show()
--- tests/cases/str_enum.py
+++ tests/cases/str_enum.py
@@ -1,7 +1,17 @@
-from enum import Enum, IntFlag, auto
+from enum import Enum
 
 
 class Colors(str, Enum):
     RED = "red"
     GREEN = "green"
     BLUE = "blue"
+
+
+def show():
+    print(Colors.RED.value)
+    print(Colors.GREEN.value)
+    print(Colors.BLUE.value)
+
+
+if __name__ == "__main__":
+    show()
