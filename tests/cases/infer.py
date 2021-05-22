aeebd14f00150cfb9acea14601a73f6127e5e69c
John Vandenberg <jayvdb@gmail.com>
2021-05-22 14:05:15 +0800

    Broken example

--- tests/cases/infer.py
+++ tests/cases/infer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-def foo():
+def infer_int():
     a = 10
     # infer that b is an int
     b = a
@@ -9,5 +9,17 @@ def foo():
     print(b)
 
 
+def infer_arg_list(b):
+    a = 'a'
+    return b == a
+
+
+#def infer_arg_set(b):
+#    a = {'a'}
+#    return b in a
+
+
 if __name__ == "__main__":
-    foo()
+    infer_int()
+    assert infer_arg_list("a")
+    #assert infer_arg_set("a")
