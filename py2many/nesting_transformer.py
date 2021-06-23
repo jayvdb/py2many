5e172a37920bf0f743cd4ad3384546417181d4c4
John Vandenberg <jayvdb@gmail.com>
2021-06-23 18:32:50 +0800

    Partial revert

--- py2many/nesting_transformer.py
+++ py2many/nesting_transformer.py
@@ -14,27 +14,40 @@ class NestingTransformer(ast.NodeTransformer):
     def __init__(self):
         self.level = 0
 
-    def _visit_level(self, node):
+    def visit_FunctionDef(self, node):
         node.level = self.level
         self.level += 1
         self.generic_visit(node)
         self.level -= 1
         return node
 
-    def visit_FunctionDef(self, node):
-        return self._visit_level(node)
-
     def visit_ClassDef(self, node):
-        return self._visit_level(node)
+        node.level = self.level
+        self.level += 1
+        self.generic_visit(node)
+        self.level -= 1
+        return node
 
     def visit_If(self, node):
-        return self._visit_level(node)
+        node.level = self.level
+        self.level += 1
+        self.generic_visit(node)
+        self.level -= 1
+        return node
 
     def visit_While(self, node):
-        return self._visit_level(node)
+        node.level = self.level
+        self.level += 1
+        self.generic_visit(node)
+        self.level -= 1
+        return node
 
     def visit_For(self, node):
-        return self._visit_level(node)
+        node.level = self.level
+        self.level += 1
+        self.generic_visit(node)
+        self.level -= 1
+        return node
 
     def visit_Assign(self, node):
         node.level = self.level
