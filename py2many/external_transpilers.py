bb771597d0b40d508b25d0886875e704d5425d93
John Vandenberg <jayvdb@gmail.com>
2021-07-01 08:35:52 +0800

    external: Fix Haxe

--- py2many/external_transpilers.py
+++ py2many/external_transpilers.py
@@ -1,12 +1,12 @@
+import pathlib
+from subprocess import run
+
 from .clike import CLikeTranspiler
 from .transpile import transpile_one
-
-from pygo.transpiler import GoTranspiler
 
 
 class HaxeTranspiler(CLikeTranspiler):
     NAME = "haxe"
-    BASE = GoTranspiler
     _target_extension = ".hx"
     _cmd = ["lix", "run", "go2hx"]
 
@@ -14,31 +14,35 @@ class HaxeTranspiler(CLikeTranspiler):
         self._intermediate_settings = intermediate_settings
         self._extension = False
         self._temp = 0
+        self._main_signature_arg_names = set()
+
+    def _get_generated_filename(self, intermediate_path):
+        CWD = pathlib.Path.cwd()
+        return CWD / "golibs" / "command_line_arguments" / (str.title(intermediate_path.stem) + self._target_extension)
 
     def visit(self, tree):
-        intermediate_path = tree.__file__.with_suffix(self._intermediate_settings)
+        intermediate_path = tree.__file__.with_suffix(self._intermediate_settings.ext)
+        print(intermediate_path)
+        print(self._intermediate_settings)
         intermediate_source = transpile_one(
             [tree],
             tree,
+            self._intermediate_settings.transpiler,
             self._intermediate_settings,
         )
         with open(intermediate_path, "w") as f:
             f.write(intermediate_source)
-        final_file = intermediate_path.with_suffix(self._target_extension)
+        assert "?" not in intermediate_source, "? found"
+        generated_file = self._get_generated_filename(intermediate_path)
         # Use _create_cmd
-        proc = run([*self._cmd])
+        cmd = [*self._cmd, str(intermediate_path)]
+        print(cmd)
+        proc = run(cmd)
         if proc.returncode:
             print(proc.stdout)
             print(proc.stderr)
-            raise NotImplementedError(f"dart2js exit {proc.returncode}")
-        with open(final_file) as f:
-            generated_by = f.readline()
-            if not generated_by:
-                raise NotImplementedError("dart2js did not generate output")
-            version_start = generated_by.find(" version")
-            assert version_start != -1
-            generated_by = generated_by[:version_start] + "\n"
+            raise NotImplementedError(f"{self._cmd[0]} exit {proc.returncode}")
+        with open(generated_file) as f:
             lines = f.readlines()
             assert lines
-            result = "\n".join(line.rstrip() for line in lines)
-            return generated_by + result
+            return "\n".join(line.rstrip() for line in lines)
