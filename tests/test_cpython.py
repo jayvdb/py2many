28ff861cfc2f66576bda20772442a0165f762677
John Vandenberg <jayvdb@gmail.com>
2021-07-07 06:13:47 +0800

    rig: skip one

--- tests/test_cpython.py
+++ tests/test_cpython.py
@@ -61,6 +61,8 @@ class CPythonTests(unittest.TestCase):
             raise unittest.SkipTest("causes RecursionError")  # https://github.com/adsharma/py2many/issues/376
         if SHOW_ERRORS and lang in ["kotlin"] and filename == "test_print":
             raise unittest.SkipTest("AttributeError: 'If' object has no attribute 'body_vars'")
+        if SHOW_ERRORS and lang in ["julia"] and filename == "test_codeccallbacks":  # other langs fail earlier with bytes <class 'ast.Mult'> int
+            raise unittest.SkipTest("ValueError: source code string cannot contain null bytes")
 
         filename += ".py"
 
