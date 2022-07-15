f5cd3b5a40a0730b116c3526af7abb6565f7eddb
John Mark Vandenberg <jayvdb@gmail.com>
2022-07-15 19:46:19 +0800

    Remove win32 special cases

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -466,12 +466,8 @@ class CodeGeneratorTests(unittest.TestCase):
 
         try:
             rv = main(args=args, env=env)
-            if sys.platform == "win32" and rv != 0:
-                raise unittest.SkipTest("transpiler failed")
             assert rv == 0
             output = filecmp.dircmp(expected_output_dir, output_dir)
-            if sys.platform == "win32" and output.diff_files != []:
-                raise unittest.SkipTest("directories don't match")
             assert output.diff_files == []
         finally:
             if not self.KEEP_GENERATED:
